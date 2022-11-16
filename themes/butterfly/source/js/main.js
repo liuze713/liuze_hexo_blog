a1dd09f845fc439b7a544270478f71fe242a20ba
liuze <liuze713@gmail.com>
2022-11-16 14:53:07 +0800

    提交

--- themes/butterfly/source/js/main.js
+++ themes/butterfly/source/js/main.js
@@ -716,13 +716,22 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
-console.log = function () {
-}
-
-function rclick() {
-  if (document.all) {
-    if (event.button == 2) {
-      event.returnvalue = false;
-    }
-  }
-}
+// console.log = function () {
+// }
+// var arr = [123, 17, 18];
+// document.oncontextmenu = new Function("event.returnValue=false;"),//禁用右键
+//   window.onkeydown = function (e) {
+//     var keyCode = e.keyCode || e.which || e.charCode;
+//     var ctrlKey = e.ctrlKey || e.metaKey;
+//     // console.log(keyCode + "--" + keyCode);
+//     if (ctrlKey && keyCode == 85) {
+//       // console.log("ctrl+u");
+//       e.preventDefault();
+//     }
+//     if (arr.indexOf(keyCode) > -1) {
+//       // console.log("其他");
+//       e.preventDefault();
+//     }
+//     //e.preventDefault();
+//     //return false;
+//   }
